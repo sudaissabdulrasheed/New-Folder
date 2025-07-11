ca884bffcc3e9c4e013222732a7e58203b2b11ab
sudaissabdulrasheed <sudaissabdulrasheed@gmail.com>
2025-07-12 00:33:02 +0100

    Sud's Bites

---
+++ script.js
@@ -0,0 +1,7 @@
+function scrollToBuy() {
+  document.getElementById("buy").scrollIntoView({ behavior: "smooth" });
+}
+
+function showPayment() {
+  document.getElementById("paymentSection").style.display = "block";
+}
